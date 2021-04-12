fdc9048b094aa9c16a6b7b33fbf3ab0281b715b1
Asi <asierrosa@gmail.com>
2021-04-12 12:02:11 +0200

    amaituta

--- main.py
+++ main.py
@@ -1,10 +1,57 @@
 import urllib
 
 import requests
-import sys
+import os;
 
+from bs4 import BeautifulSoup
 
-def lehenegoEskaera():
+cookie = ""
+
+def pdf_deskargatu(uria,izena):
+    izena = str(izena).replace(' ', '_')
+    metodoa = "GET"
+    goiburuak = {'Host': 'egela.ehu.eus', 'Cookie': cookie}
+    res = requests.request(metodoa, uria, headers=goiburuak, allow_redirects=False)
+    pdf = res.content
+    file = open("./pdfs/" + str(izena) + ".pdf", "wb")
+    file.write(pdf)
+    file.close()
+
+def pdf_joan(content):
+    soup = BeautifulSoup(content, 'html.parser')
+    div_list = soup.find('div', {'class': 'resourceworkaround'})
+    uria = div_list.find('a').get('href')
+    list = uria.split('/')
+    izena = list[len(list)-1]
+    print(izena)
+    pdf_deskargatu(uria,izena)
+
+def irakasgaiko_pdf(uria):
+    metodoa = "GET"
+    goiburuak = {'Host': 'egela.ehu.eus', 'Cookie': cookie}
+    erantzuna = requests.request(metodoa, uria, headers=goiburuak, allow_redirects=False)
+    html_kodea = erantzuna.content
+    soup = BeautifulSoup(html_kodea, 'html.parser')
+
+    div_list = soup.find_all('div', {'class': 'activityinstance'})
+    for elem in div_list:
+        if "pdf" in str(elem.find('img')['src']):
+            izena = elem.find("span", {"class": "instancename"}).text
+            uria = elem.find('a').get('href')
+            erantzuna = requests.request(metodoa, uria, headers=goiburuak, allow_redirects=False)
+            pdf_joan(erantzuna.content)
+
+def sartuIrakasgaian(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    a_links = soup.find_all("a", {'class': 'ehu-visible'})
+    for elem in a_links:
+        if elem.text == "Web Sistemak":
+            u = elem.get('href')
+            break
+    irakasgaiko_pdf(u)
+
+def sartuEgelan(erabiltzailea,pasahitza):
+    global cookie
     metodo = 'GET'
     uria = "https://egela.ehu.eus/"
     goiburuak = {'Host': 'egela.ehu.eus'}
@@ -12,7 +59,7 @@ def lehenegoEskaera():
     erantzuna = requests.request(metodo, uria, headers=goiburuak, data=edukia, allow_redirects=False)
 
     cookie = erantzuna.headers['Set-Cookie'].split(";")[0]
-    host = erantzuna.url.split("/")[2]
+    host = "egela.ehu.eus"
 
     print("#######################################################")
     print("LEHENENGO ESKAERA")
@@ -22,24 +69,19 @@ def lehenegoEskaera():
     print("edukia: " + edukia)
     print("host: " + host)
 
-
-
-    #Esto para decir que tipo de respuesta hay
+    if 'Location' in erantzuna.headers:
+        uria = erantzuna.headers['Location']
+    if 'Set-Cookie' in erantzuna.headers:
+        cookie = erantzuna.headers['Set-Cookie'].split(",")[0]
     codigo = erantzuna.status_code
     descripcion = erantzuna.reason
     print(str(codigo) + " " + descripcion)
 
     print("#######################################################")
 
-    return cookie,host
-
-
-def bigarrenEskaera(cookie,host,erabiltzailea,pasahitza):
     metodo = 'POST'
     uria = "https://"+host+"/login/index.php"
-    cookieIzenburua = cookie.split("=")[0]
-    cookieKodea = cookie.split("=")[1]
-    goiburuak = {'Host':host,cookieIzenburua:cookieKodea,
+    goiburuak = {'Host':host,'Cookie':cookie,
                  'Content-Type':'application/x-www-form-urlencoded',
                  'Content-Length':'0'}
     edukia = {'username':erabiltzailea,'password':pasahitza}
@@ -47,9 +89,12 @@ def bigarrenEskaera(cookie,host,erabiltzailea,pasahitza):
     goiburuak['Content-Length'] = str(len(edukia_encoded))
     erantzuna = requests.request(metodo, uria, headers=goiburuak, data=edukia_encoded, allow_redirects=False)
 
-    print("#######################################################")
-    print("-------------------------------------------------------")
-    print("BIGARREN ESKAERAREN LEHENGO HTTP ESKAERA (POST)")
+    if 'Location' in erantzuna.headers:
+        uria = erantzuna.headers['Location']
+    if 'Set-Cookie' in erantzuna.headers:
+        cookie = erantzuna.headers['Set-Cookie'].split(",")[0]
+
+
     print("metodoa: " + metodo)
     print("uria: " + uria)
     print("edukia: " + edukia_encoded)
@@ -58,47 +103,17 @@ def bigarrenEskaera(cookie,host,erabiltzailea,pasahitza):
     descripcion = erantzuna.reason
     print(str(codigo) + " " + descripcion)
 
-    print("-------------------------------------------------------")
-
-
-    print("BIGARREN ESKAERAREN BIGARREN HTTP ESKAERA (GET)")
-    metodo = 'GET'
-    uria = erantzuna.headers['Location']
-    goiburuak = {'Host': host, cookieIzenburua: cookieKodea}
-    edukia = ''
-    erantzuna = requests.request(metodo, uria, headers=goiburuak, data=edukia, allow_redirects=False)
-
-    print("metodoa: " + metodo)
-    print("uria: " + uria)
-    print("edukia: " + edukia)
-
-    codigo = erantzuna.status_code
-    descripcion = erantzuna.reason
-    print(str(codigo) + " " + descripcion)
-
-    print("-------------------------------------------------------")
-
     print("#######################################################")
 
-    #Aqui hay que pasarle el id de web sistemak pero nose como se hace eso
-    return cookieIzenburua,cookieKodea,42336
-
-
-
-def hirugarrenEskaera(cookieIzenburua,cookieKodea,id):
     metodo = 'GET'
-    uria = "https://"+host+"/course/view.php"
-    goiburuak = {'Host':host, cookieIzenburua:cookieKodea}
-    edukia = {'id':id}
-    edukia_encoded = urllib.parse.urlencode(edukia)
-    erantzuna = requests.request(metodo, uria, headers=goiburuak, data=edukia_encoded, allow_redirects=False)
+    goiburuak = {'Host':host, 'Cookie':cookie}
+    edukia = ''
+    erantzuna = requests.request(metodo, uria, headers=goiburuak, data=edukia, allow_redirects=False)
 
     print("HIRUGARREN ESKAERA")
     print("metodoa: " + metodo)
     print("uria: " + uria)
-    print("edukia: " + edukia_encoded)
-
-
+    print("edukia: " + edukia)
 
     codigo = erantzuna.status_code
     descripcion = erantzuna.reason
@@ -106,13 +121,13 @@ def hirugarrenEskaera(cookieIzenburua,cookieKodea,id):
 
     print("#######################################################")
 
-
-def laugarrenEskaera(cookieIzenburua,cookieKodea):
     metodo = 'GET'
     uria = "https://egela.ehu.eus/"
-    goiburuak = {'Host': host, cookieIzenburua: cookieKodea}
+    goiburuak = {'Host': host, 'Cookie':cookie}
     edukia = ''
     erantzuna = requests.request(metodo, uria, headers=goiburuak, data=edukia, allow_redirects=False)
+
+    print("LAUGARREN ESKAERA")
 
     print("metodoa: " + metodo)
     print("uria: " + uria)
@@ -121,16 +136,17 @@ def laugarrenEskaera(cookieIzenburua,cookieKodea):
     codigo = erantzuna.status_code
     descripcion = erantzuna.reason
     print(str(codigo) + " " + descripcion)
+    sartuIrakasgaian(erantzuna.content)
+
 
 
 if __name__ == '__main__':
-    cookie,host = lehenegoEskaera()
 
-    print("Mesedez sartu ezazu zure erabiltzailea eta pasahitza","911806","Ani0045")
-    cookieIzenburua,cookieKodea,id = bigarrenEskaera(cookie,host,"911806","Ani0045")
-    hirugarrenEskaera(cookieIzenburua,cookieKodea,id)
-    laugarrenEskaera(cookieIzenburua,cookieKodea)
+    try:
+        os.mkdir('pdfs')
+    except OSError as e:
+        print("Jada karpeta sortuta dago")
 
-
-    # print("Mesedez sartu ezazu zure erabiltzailea eta pasahitza",sys.argv[0],sys.argv[1])
-    # bigarrenEskaera(cookie,host,sys.argv[0],sys.argv[1])
+    username = input("Sar ezazu zure erabiltzailea\n")
+    password = input("Sart ezazu zure pasahitza\n")
+    sartuEgelan(username,password)
